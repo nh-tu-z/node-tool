e474b4bee0733176e16585f62ebee4e2b34479da
tuhngo <nhtu1611@gmail.com>
2023-07-01 21:23:05 +0700

    seriallint

---
+++ seriallint/AdvSerialCommunicator/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using AdvSerialCommunicator.Serial;
+using AdvSerialCommunicator.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AdvSerialCommunicator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            ((MainViewModel)DataContext).SerialPort.CloseAll();
+        }
+    }
+}
---
+++ seriallint/AdvSerialCommunicator/ViewModels/MainViewModel.cs
@@ -0,0 +1,42 @@
+﻿using AdvSerialCommunicator.Messaging;
+using AdvSerialCommunicator.Serial;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheRFramework.Utilities;
+
+namespace AdvSerialCommunicator.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public MessagesViewModel Messages { get; set; }
+
+        public MessageReceiver Receiver { get; set; }
+
+        public MessageSender Sender { get; set; }
+
+        public SerialPortViewModel SerialPort { get; set; }
+
+        public MainViewModel()
+        {
+            SerialPort = new SerialPortViewModel();
+            Receiver = new MessageReceiver();
+            Sender = new MessageSender();
+            Messages = new MessagesViewModel();
+
+            // hmm
+            Receiver.Messages = Messages;
+            Messages.Sender = Sender;
+            Sender.Messages = Messages;
+
+            SerialPort.Receiver = Receiver;
+            SerialPort.Sender = Sender;
+            SerialPort.Messages = Messages;
+
+            Receiver.Port = SerialPort.Port;
+            Sender.Port = SerialPort.Port;
+        }
+    }
+}
